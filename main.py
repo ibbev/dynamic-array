4fced352ae8b97302e0dc299fb4696811750462c
ibbev <ibbe@ten-veen.nl>
2026-09-25 09:38:09 +0200

    basics

---
+++ main.py
@@ -0,0 +1,17 @@
+links = []
+rechts = []
+i = 0
+lijst = [0,6,5,3,4,6]
+
+
+while True:
+	if len(lijst) < 2:
+		print(lijst)
+		break
+	else:
+		pass
+	if i < len(lijst):
+		merge_sort(links)
+		merge_sort(rechts)
+		
+
